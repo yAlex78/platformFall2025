342814821f90f27e87f8a9ce147d3a3d0cfdc982
yAlex78 <85961347+yAlex78@users.noreply.github.com>
2025-11-12 15:05:49 -0800

    feat: Added endpoints and controllers for PUT, POST, and DELETE

--- backend/routes/todosRouter.ts
+++ backend/routes/todosRouter.ts
@@ -1,8 +1,11 @@
 import { Router, type Request, type Response } from "express";
-import { getTodoController } from "../controllers/todosControllers.ts";
+import { getTodoController, createTodoController, deleteTodoController, updateTodoController } from "../controllers/todosControllers.ts";
 
 const todosRouter = Router()
 
 todosRouter.get("/:id", (req: Request, res: Response) => getTodoController(req, res));
+todosRouter.post("/", (req: Request, res: Response) => createTodoController(req, res));
+todosRouter.delete("/:id", (req: Request, res: Response) => deleteTodoController(req, res));
+todosRouter.put("/:id", (req: Request, res:Response) => updateTodoController(req, res));
 
 export default todosRouter;
