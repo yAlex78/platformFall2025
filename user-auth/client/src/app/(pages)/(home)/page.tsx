e92daf67fe1fb18f76ca0a1fb06dffaedd3ffc77
erikagosti <erika.gosti@gmail.com>
2025-11-25 23:29:23 -0800

    added log in redirection if user not authenticated

--- user-auth/client/src/app/(pages)/(home)/page.tsx
+++ user-auth/client/src/app/(pages)/(home)/page.tsx
@@ -1,9 +1,16 @@
+"use client";
+import { useEffect } from "react";
+import Cookies from "js-cookie";
+import { useRouter } from "next/navigation";
 import styles from "./page.module.scss";
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      hello world
-    </div>
-  );
+  const router = useRouter();
+  useEffect(() => {
+    const token = Cookies.get("auth_token");
+    if (!token) {
+      router.replace("/log-in");
+    }
+  }, [router]);
+  return <div className={styles.page}>hello world</div>;
 }
--- user-auth/client/src/app/(pages)/log-in/page.tsx
+++ user-auth/client/src/app/(pages)/log-in/page.tsx
@@ -1,13 +1,24 @@
-'use client'
+"use client";
 import { useState } from "react";
 import styles from "./page.module.scss";
 import logIn from "@/app/(util)/logIn";
 import Link from "next/link";
 import Cookies from "js-cookie";
+import { useRouter } from "next/navigation";
 
 export default function LogIn() {
   const [usernameInput, setUsernameInput] = useState<string>("");
   const [passwordInput, setPasswordInput] = useState<string>("");
+  const router = useRouter();
+
+  const handleLogin = async () => {
+    try {
+      await logIn(usernameInput, passwordInput);
+      router.push("/secret");
+    } catch (e) {
+      alert("Login failed");
+    }
+  };
 
   return (
     <div className={styles.page}>
@@ -16,20 +27,28 @@ export default function LogIn() {
         <label>Username</label>
         <input
           value={usernameInput}
-          onChange={(e) => {setUsernameInput(e.target.value)}}
+          onChange={(e) => {
+            setUsernameInput(e.target.value);
+          }}
         />
       </div>
       <div>
         <label>Password</label>
         <input
           value={passwordInput}
-          onChange={(e) => {setPasswordInput(e.target.value)}}
+          onChange={(e) => {
+            setPasswordInput(e.target.value);
+          }}
         />
       </div>
-      <button onClick={() => logIn(usernameInput, passwordInput)}>Log In</button>
-      <button onClick={() => {
-        console.log(Cookies.get("auth_token"));
-      }}>Check token</button>
+      <button onClick={handleLogin}>Log In</button>
+      <button
+        onClick={() => {
+          console.log(Cookies.get("auth_token"));
+        }}
+      >
+        Check token
+      </button>
       <Link href="/secret">go to secret page</Link>
     </div>
   );
